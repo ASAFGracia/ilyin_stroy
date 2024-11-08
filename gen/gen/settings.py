3eff05d9a367b9ae24f57e56892fbea3375c27b7
asaf <ilyin.rostislaw@gmai.com>
2024-11-08 20:52:08 +0300

    выгрузка на хост 3

--- gen/gen/settings.py
+++ gen/gen/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-7af$b@npurf1ue_*v(x38@#2(sns(une*(46m*(@_qlqd*&_9k'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ilyin-stroy.xyz']
 
