5afcd2be68b04360714bfea221ce5b9e0a54b2ca
asaf <ilyin.rostislaw@gmai.com>
2024-11-09 17:00:08 +0300

    выгрузка на хост 4

--- gen/gen/asgi.py
+++ gen/gen/asgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gen.gen.settings')
 
 application = get_asgi_application()
