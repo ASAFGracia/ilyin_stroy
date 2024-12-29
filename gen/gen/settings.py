d1d6f3c8091b28671b1b1050b7a848d33820167a
ASAFGracia <98877353+ASAFGracia@users.noreply.github.com>
2024-12-29 16:07:59 +0300

    Update settings.py

--- gen/gen/settings.py
+++ gen/gen/settings.py
@@ -21,7 +21,7 @@ EMAIL_HOST_USER = 'ilyinstroy@gmail.com'
 EMAIL_HOST_PASSWORD = 'aokp ytxy klie klad '
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-STATIC_ROOT = "/root/ilyin_stroy/gen/staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
