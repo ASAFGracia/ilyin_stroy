96c0ea6fc5d8b438cfa7c2ebe21e560f073ac45e
asaf <ilyin.rostislaw@gmai.com>
2025-01-06 09:54:13 +0300

    NEW LOGO!!!

--- gen/gen/settings.py
+++ gen/gen/settings.py
@@ -66,22 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gen.wsgi.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 DATABASES = {
     'default': {
