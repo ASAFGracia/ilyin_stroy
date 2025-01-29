aa52a95a5be839a97341db82480e390428fa7a88
asaf <ilyin.rostislaw@gmail.com>
2025-01-30 02:32:37 +0300

    добавил новое лого и исправляю моб вёрстку

--- gen/gen/settings.py
+++ gen/gen/settings.py
@@ -10,7 +10,6 @@ SECRET_KEY = 'django-insecure-7af$b@npurf1ue_*v(x38@#2(sns(une*(46m*(@_qlqd*&_9k
 DEBUG = False
 
 
-
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ilyin-stroy.xyz', 'www.ilyin-stroy.xyz']
 
 
