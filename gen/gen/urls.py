d2348c4e60bea306edc5943e6a6117fb4ab984a9
server <server@Mac-mini-Asaf.local>
2026-02-13 00:28:58 +0300

    chore: remove public admin route and rely on owner panel

--- gen/gen/urls.py
+++ gen/gen/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
@@ -11,7 +10,6 @@ sitemaps = {
 }
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("base.urls")),
     path("prices/", include("prices.urls")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
